ea796e77e260f560b321cd8a3fe8976dc833ccd5
Hayley Gelder <63299952+HGelder@users.noreply.github.com>
2020-04-28 16:36:52 +0100

    Added file player1
    Added players 1 and 2 and keyboard layouts to handle movement/shooting. Also added player health spritesheet and animations. Player 2 does not yet have a file, as when player 1 is complete it can be used to create player 2 file.

--- game.js
+++ game.js
@@ -15,6 +15,22 @@ var backgroundGame;
 var buttonPlay;
 var labelPlayer1;
 var labelPlayer2;
+var healthPlayer1;
+var healthPlayer2;
+
+var player1;
+var p1Up;
+var p1Down;
+var p1Left;
+var p1Right;
+var p1Shoot;
+
+var player2;
+var p2Up;
+var p2Down;
+var p2Left;
+var p2Right;
+var p2Shoot;
 
 function preload(){
     
@@ -24,6 +40,10 @@ function preload(){
     
     game.load.image('labelPlayer1', 'assets/labelPlayer1.png');
     game.load.image('labelPlayer2', 'assets/labelPlayer2.png');
+    game.load.image('player1' , 'assets/playerTemplate.png');
+    game.load.image('player2' , 'assets/playerTemplate.png');
+    
+    game.load.spritesheet('spritesheetHealth', 'assets/spritesheetHealth.png', 180, 60, 7);
       
 }
 
@@ -45,12 +65,44 @@ function create(){
     labelPlayer1.anchor.setTo(0.5, 0.5);
     labelPlayer2.anchor.setTo(0.5, 0.5);
     
+    healthPlayer1 = game.add.sprite(width/2 + 380, height/7, 'spritesheetHealth');
+    healthPlayer1.anchor.setTo(0.5, 0.5);
+    healthPlayer1.animations.add('healthAnimation', [0, 1, 2, 3, 4, 5, 6]);
+    healthPlayer1.frame = 0;
+    healthPlayer1.visible = false;
+    
+    healthPlayer2 = game.add.sprite(width/2 - 380, height/7, 'spritesheetHealth');
+    healthPlayer2.anchor.setTo(0.5, 0.5);
+    healthPlayer2.animations.add('healthAnimation', [0, 1, 2, 3, 4, 5, 6]);
+    healthPlayer2.frame = 0;
+    healthPlayer2.visible = false;
+    
+    player1 = game.add.sprite(width - 820, height/2, 'player1');
+    player1.anchor.setTo(0.5, 0.5);
+    player1.visible = false;
+    p1Up = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    p1Down = game.input.keyboard.addKey(Phaser.Keyboard.S);
+    p1Left = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    p1Right = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    p1Shoot = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    
+    player2 = game.add.sprite(width - 140, height/2, 'player2');
+    player2.anchor.setTo(0.5, 0.5);
+    player2.visible = false;
+    p2Up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    p2Down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    p2Left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    p2Right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    p2Shoot = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    
 }
 
 function update(){
     
     if (playing){
-        // do stuff
+        
+        updateP1();
+        
     }
     
 }
@@ -63,6 +115,10 @@ function play(){
     backgroundGame.visible = true;
     labelPlayer1.visible = true;
     labelPlayer2.visible = true;
+    healthPlayer1.visible = true;
+    healthPlayer2.visible = true;
+    player1.visible = true;
+    player2.visible = true;
     
 }
 
@@ -74,5 +130,9 @@ function exit(){
     backgroundGame.visible = false;
     labelPlayer1.visible = false;
     labelPlayer2.visible = false;
+    healthPlayer1.visible = false;
+    healthPlayer2.visible = false;
+    player1.visible = false;
+    player2.visible = false;
     
 }
